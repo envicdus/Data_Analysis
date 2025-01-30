7d50378ac6b7a950e79f74ed4c18121a05ec0098
Neil Vince Decio <89781502+envicdus@users.noreply.github.com>
2025-01-30 21:31:06 +0800

    added app.py for streamlit

---
+++ Philippine_food_price_Analysis_and_Visualization/app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from nbclient import NotebookClient
+from nbformat import read
+
+# Path to your Jupyter notebook
+notebook_path = '/Reports/full_report.ipynb'
+
+# Read and execute the notebook
+with open(notebook_path) as f:
+    nb = read(f, as_version=4)
+    client = NotebookClient(nb)
+    client.execute()
+
+# Now you can display output from the notebook in Streamlit
+st.write("Notebook executed successfully")
